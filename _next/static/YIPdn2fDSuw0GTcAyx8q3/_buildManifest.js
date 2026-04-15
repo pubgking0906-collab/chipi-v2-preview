e28879afe7aafd29bda76ca67c238607d78ad5f3
Oded Hogeg <oded@Odeds-Mac-mini.local>
2026-04-15 13:10:10 -0700

    Updates

---
+++ _next/static/YIPdn2fDSuw0GTcAyx8q3/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/chipi-v2-preview//_next/:path+",
+        "destination": "/chipi-v2-preview/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
